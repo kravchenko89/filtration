459946bdaedf0e1c67f01b9f7ced39fb7a237073
kravchenko89 <symkin2007@gmail.com>
2020-01-29 19:40:04 +0200

    test

--- filt/humans/tests.py
+++ filt/humans/tests.py
@@ -1,1 +1,25 @@
+from django.test import TestCase
+from django.shortcuts import reverse
+
+from humans.models import *
+
+
+class TestUrls(TestCase):
+    def test_urls1(self):
+        response = self.client.get(reverse('filt-teacher'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_urls2(self):
+        response = self.client.get(reverse('filt-group'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_urls3(self):
+        response = self.client.get(reverse('filt-group'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'groups_list.html')
+
+
+class Teacher(TestCase):
+    def teach_test(self):
+        teacher = Teacher.generate_teacher()
 
